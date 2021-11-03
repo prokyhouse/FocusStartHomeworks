c5ed1d8ac52e0d202830dff9c9ca2995c12b41a6
Кирилл Проко <prokyhouse@mail.ru>
2021-11-03 22:26:49 +0700

    (HomeWork-2) Создал класс ThreadSafeArray

---
+++ ThreadSafeArray/ThreadSafeArray/ThreadSafeArray.swift
@@ -0,0 +1,13 @@
+//
+//  ThreadSafeArray.swift
+//  ThreadSafeArray
+//
+//  Created by Кирилл Прокофьев on 03.11.2021.
+//
+
+import Foundation
+import Dispatch
+
+public final class ThreadSafeArray<T> {
+
+}
