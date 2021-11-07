13aefe905d1e471888d112cf90f9ab8eda384dff
Кирилл Проко <prokyhouse@mail.ru>
2021-11-08 01:22:03 +0700

    (HomeWork-2) Реализовал ThreadSafeArray

--- ThreadSafeArray/ThreadSafeArray/ThreadSafeArray.swift
+++ ThreadSafeArray/ThreadSafeArray/ThreadSafeArray.swift
@@ -9,5 +9,46 @@ import Foundation
 import Dispatch
 
 public final class ThreadSafeArray<T> {
+	private var array = [T]()
+	
+	private let queue = DispatchQueue(label: "ThreadSafeArrayQueue",
+									  attributes: .concurrent)
+	
+	func append(_ item: T) {
+		queue.async(flags: .barrier) { [weak self] in
+			self?.array.append(item)
+		}
+	}
+	
+	func remove(at index: Int) {
+		queue.async(flags: .barrier) { [weak self] in
+			self?.array.remove(at: index)
+		}
+	}
+	
+	var isEmpty: Bool {
+		queue.sync {
+			return self.array.isEmpty
+		}
+	}
+	
+	var count: Int {
+		queue.sync {
+			return self.array.count
+		}
+	}
+	
+	subscript(index: Int) -> T {
+		queue.sync {
+			return self.array[index]
+		}
+	}
+}
 
+extension ThreadSafeArray where T: Equatable {
+	func contains(_ element: T) -> Bool {
+		queue.sync {
+			return (self.array.firstIndex(of: element) != nil) ? true : false
+		}
+	}
 }
--- ThreadSafeArray/ThreadSafeArray/main.swift
+++ ThreadSafeArray/ThreadSafeArray/main.swift
@@ -7,3 +7,32 @@
 
 import Foundation
 
+var threadSafeArray = ThreadSafeArray<String>()
+
+let queue = DispatchQueue(label: "ThreadSafeArrayQueue",
+							  attributes: .concurrent)
+
+let group = DispatchGroup.init()
+
+queue.async(group: group) {
+	for i in 0...1000 {
+		threadSafeArray.append("X-\(i)")
+	}
+}
+
+queue.async(group: group) {
+	for i in 0...1000 {
+		threadSafeArray.append("Y-\(i)")
+	}
+}
+
+group.notify(queue: .main) {
+	if (threadSafeArray.isEmpty) {
+		print("Array is empty.")
+	} else {
+		print("Number of elements: \(threadSafeArray.count)")
+	}
+	exit(EXIT_SUCCESS)
+}
+
+dispatchMain()
