c6bd79ce2e298858154b19f20da6673ed1c1a0b9
Кирилл Проко <prokyhouse@mail.ru>
2021-11-15 22:27:11 +0700

    (HomeWork-3) Починил баг с перерисовкой градиента при ротации

--- myCV/myCV/Extensions/UIView+Gradient.swift
+++ myCV/myCV/Extensions/UIView+Gradient.swift
@@ -9,6 +9,11 @@ import UIKit
 
 extension UIView {
 	func setGradientBackground(colorTop: UIColor, colorBottom: UIColor, cornerRadius: CGFloat) {
+
+		if let _ = (self.layer.sublayers?.compactMap { $0 as? CAGradientLayer })?.first {
+			removeSublayer(self, layerIndex: 0)
+		}
+		
 		let gradientLayer = CAGradientLayer()
 		gradientLayer.frame = self.bounds
 		gradientLayer.colors = [colorTop.cgColor, colorBottom.cgColor]
@@ -16,6 +21,7 @@ extension UIView {
 		gradientLayer.endPoint = CGPoint(x: 0.0, y: 0.0)
 		gradientLayer.cornerRadius = cornerRadius
 		self.layer.insertSublayer(gradientLayer, at: 0)
+
 	}
 
 	func setGradientBackground(colorTop: UIColor, colorBottom: UIColor, startY: Double, endY: Double) {
@@ -25,5 +31,17 @@ extension UIView {
 		gradientLayer.startPoint = CGPoint(x: 0.0, y: startY)
 		gradientLayer.endPoint = CGPoint(x: 0.0, y: endY)
 		self.layer.insertSublayer(gradientLayer, at: 0)
+	}
+
+	func removeSublayer(_ view: UIView, layerIndex index: Int) {
+		guard let sublayers = view.layer.sublayers else {
+			print("The view does not have any sublayers.")
+			return
+		}
+		if sublayers.count > index {
+			view.layer.sublayers!.remove(at: index)
+		} else {
+			print("There are not enough sublayers to remove that index.")
+		}
 	}
 }
