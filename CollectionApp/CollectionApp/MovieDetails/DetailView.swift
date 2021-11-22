aa47b623e3fe92ca5491889c73ae50977cd86cfc
Кирилл Проко <prokyhouse@mail.ru>
2021-11-22 22:36:26 +0700

    (HomeWork-4) Убрал явную инициализацию типа

--- CollectionApp/CollectionApp/MovieDetails/DetailView.swift
+++ CollectionApp/CollectionApp/MovieDetails/DetailView.swift
@@ -10,9 +10,9 @@ import UIKit
 import SnapKit
 
 final class DetailView: UIView {
-
-	private var contentModel: Movie = Movie(title: "smth", overview: "overview", genres: ["genre"])
-
+	
+	private var contentModel = Movie(title: "smth", overview: "overview", genres: ["genre"])
+	
 	private enum Metrics {
 		static let spaceBetweenComponents: CGFloat = 9
 		static let blurRadius: CGFloat = 16
@@ -21,79 +21,79 @@ final class DetailView: UIView {
 		static let titleSize: CGFloat = 22
 		static let numberOfLines = 4
 	}
-
+	
 	private let posterView = UIImageView()
 	private let titleLabel = UILabel()
 	private let descriptionLabel = UILabel()
 	internal let moreButton = UIButton()
 	private let contentView = UIView()
-
+	
 	public override init(frame: CGRect) {
 		super.init(frame: frame)
 		self.configure()
 	}
-
+	
 	public required init?(coder: NSCoder) {
 		fatalError("init(coder:) has not been implemented")
 	}
-
+	
 	private func configure() {
 		self.setConfig()
 		self.addSubviews()
 		self.setConstraints()
 	}
-
+	
 	private func setConfig() {
-
+		
 		self.contentView.backgroundColor = .black
 		self.contentView.layer.cornerRadius = Metrics.cornerRadius
-
+		
 		self.backgroundColor = .black
 		
 		self.posterView.contentMode = .scaleAspectFill
-
+		
 		self.titleLabel.textColor = .white
 		self.titleLabel.textAlignment = .center
 		self.titleLabel.font = UIFont.systemFont(ofSize: Metrics.titleSize, weight: UIFont.Weight.bold)
-
+		
 		self.descriptionLabel.numberOfLines = Metrics.numberOfLines
 		self.descriptionLabel.textColor = .white
-
+		
 		self.moreButton.setTitle("полное описание", for: .normal)
 		self.moreButton.setTitleColor(UIColor(named: "AccentColor"), for: .normal)
 		self.moreButton.setTitleColor(.white, for: .highlighted)
 		self.moreButton.titleLabel?.adjustsFontSizeToFitWidth = true
 	}
-
+	
 	func setContent(model: Movie) {
 		self.contentModel = model
 		self.posterView.image = UIImage(named: model.title)?.blurred(radius: Metrics.blurRadius)
 		self.titleLabel.text = model.title
 		self.descriptionLabel.text = model.overview
 	}
-
+	
 	private func addSubviews() {
 		self.addSubview(self.posterView)
 		self.addSubview(self.contentView)
 		self.contentView.addSubview(self.descriptionLabel)
 		self.contentView.addSubview(self.moreButton)
 	}
-
+	
 	private func setConstraints() {
 		self.posterView.snp.makeConstraints { make in
 			make.left.right.top.bottom.equalToSuperview()
 		}
-
+		
 		self.contentView.snp.makeConstraints { make in
 			make.centerY.centerX.equalToSuperview()
 			make.right.left.equalToSuperview().inset(Metrics.contentInset)
 			make.bottom.equalTo(self.moreButton).offset(Metrics.spaceBetweenComponents)
 		}
-
+		
 		self.descriptionLabel.snp.makeConstraints { make in
 			make.top.left.right.equalTo(self.contentView).inset(Metrics.spaceBetweenComponents)
 		}
-
+		
 		self.moreButton.snp.makeConstraints { make in
 			make.centerX.equalTo(self.contentView)
 			make.top.equalTo(self.descriptionLabel.snp.bottom).offset(Metrics.spaceBetweenComponents)
--- CollectionApp/CollectionApp/MovieDetails/DetailViewController.swift
+++ CollectionApp/CollectionApp/MovieDetails/DetailViewController.swift
@@ -9,25 +9,25 @@ import Foundation
 import UIKit
 
 final class DetailViewController: UIViewController {
-	var movie: Movie = Movie(title: "smth", overview: "text", genres: ["no genre"])
+	var movie = Movie(title: "smth", overview: "text", genres: ["no genre"])
 	private let detailView = DetailView()
-
+	
 	override func loadView() {
 		detailView.setContent(model: movie)
 		self.view = self.detailView
 	}
-
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		self.title = movie.title
 		self.detailView.moreButton.addTarget(self, action: #selector(openOverview), for: .touchUpInside)
 	}
-
+	
 	override func viewWillAppear(_ animated: Bool) {
 		super.viewWillAppear(animated)
 		self.tabBarController?.tabBar.isHidden = true
 	}
-
+	
 	@IBAction func openOverview(_ sender: Any) {
 		let overviewVC = OverviewViewController()
 		overviewVC.modalPresentationStyle = .pageSheet
@@ -35,14 +35,14 @@ final class DetailViewController: UIViewController {
 		overviewVC.movie = movie
 		present(overviewVC, animated: true)
 	}
-
+	
 	private func configure() {
 		self.title = "Details"
 		self.view.backgroundColor = .systemBackground
-
+		
 		let label = UILabel()
 		label.text = "SOME DETAILS"
-
+		
 		self.view.addSubview(label)
 		label.pinToSuperviewCenter()
 	}
